b4124e1228651e47e555091ca966d5092894478b
patti-c <patricia.carter@flatironschool.com>
2018-09-03 20:58:00 -0400

    Done.

--- spec/03_update_spec.rb
+++ spec/03_update_spec.rb
@@ -7,7 +7,7 @@ describe 'updating' do
     @sql_runner.execute_update_sql
   end
 
-  it 'updates the species of the last character in the characters table to "Martian"' do 
+  it 'updates the species of the last character in the characters table to "Martian"' do
     expect(@db.execute('SELECT species FROM characters WHERE id=(SELECT MAX(id) FROM characters);').flatten[0]).to eq("Martian")
   end
 end
